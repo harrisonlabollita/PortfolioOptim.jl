4297bdcc1f2004fc0935f4c0787b22ff3e86d9e1
harrisonlabollita <hlabollita0219@lions.piedmont.edu>
2022-08-01 12:19:56 -0700

    fix workflows

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,8 +1,7 @@
 using PortfolioOptim
 using Test
 
-@testset "PortfolioOptim.jl" begin
-	include("market_data.jl")
+@testset "PortfolioOptim.jl" begin include("market_data.jl")
 	include("portfolio.jl")
 	include("eff_frontier.jl")
 end
